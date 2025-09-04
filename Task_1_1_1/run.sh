7d3ade77acb408a1da1f477cc0ad50c7e4e97a50
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2025-09-05 00:48:55 +0700

    add custom build script

---
+++ Task_1_1_1/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT_DIR=$(dirname $(realpath $0))
+SRC_DIR=$PROJECT_DIR/src/main/java
+BUILD_DIR=$PROJECT_DIR/build/custom
+CLASS_DIR=$BUILD_DIR/classes
+DOC_DIR=$BUILD_DIR/docs
+JAR=$BUILD_DIR/App.jar
+JAVA_FILES=$(find $SRC_DIR -name '*.java')
+
+javac $JAVA_FILES -d $CLASS_DIR
+cd $CLASS_DIR
+jar cfe $JAR ru.nsu.aeliseev2.task_1_1_1.App $(find . -name '*.class')
+javadoc $JAVA_FILES -d $DOC_DIR
+java -jar $JAR
+
