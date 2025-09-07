d9fb770286a69173b6654b9f01c130ebba0081f4
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2025-09-07 19:22:41 +0700

    add console utils tests and more card hand tests

--- Task_1_1_2/src/test/java/ru/nsu/aeliseev2/task112/tests/ConsoleControllerTests.java
+++ Task_1_1_2/src/test/java/ru/nsu/aeliseev2/task112/tests/ConsoleControllerTests.java
@@ -2,13 +2,16 @@ package ru.nsu.aeliseev2.task112.tests;
 
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import ru.nsu.aeliseev2.task112.controllers.ConsoleController;
 import ru.nsu.aeliseev2.task112.controllers.GameAction;
 import ru.nsu.aeliseev2.task112.model.CardHand;
 
 class ConsoleControllerTests {
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
     @Test
     void stand() {
         var expected = GameAction.STAND;
@@ -21,6 +24,7 @@ class ConsoleControllerTests {
         Assertions.assertEquals(expected, controller.chooseAction(hand));
     }
 
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
     @Test
     void hit() {
         var expected = GameAction.HIT;
@@ -33,6 +37,7 @@ class ConsoleControllerTests {
         Assertions.assertEquals(expected, controller.chooseAction(hand));
     }
 
+    @Timeout(value = 100, unit = TimeUnit.MILLISECONDS)
     @Test
     void invalid() {
         var expected = GameAction.HIT;
