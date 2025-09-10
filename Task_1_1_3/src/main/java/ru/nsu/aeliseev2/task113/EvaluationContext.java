8cbd1e2171e9f5564ce664390b7082ba683e37b1
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2025-09-10 16:30:10 +0700

    add expression parser and tests for it

--- Task_1_1_3/src/main/java/ru/nsu/aeliseev2/task113/EvaluationContext.java
+++ Task_1_1_3/src/main/java/ru/nsu/aeliseev2/task113/EvaluationContext.java
@@ -1,30 +1,7 @@
 package ru.nsu.aeliseev2.task113;
 
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
 public interface EvaluationContext {
-    Pattern EQUALS_PATTERN = Pattern.compile("=");
-    Pattern SEPARATOR_PATTERN = Pattern.compile(";");
+    EvaluationContext EMPTY = name -> 0;
 
     double getVariable(String name);
-
-    static EvaluationContext scan(Scanner scanner) {
-        var context = new HashMapEvaluationContext();
-        while (scanner.hasNext()) {
-            String name = scanner.next();
-            scanner.next(EQUALS_PATTERN);
-            double value = scanner.nextDouble();
-            context.setVariable(name, value);
-            if (!scanner.hasNext(SEPARATOR_PATTERN)) {
-                break;
-            }
-            scanner.next(SEPARATOR_PATTERN);
-        }
-        return context;
-    }
-
-    static EvaluationContext fromString(String str) {
-        return scan(new Scanner(str));
-    }
 }
---
+++ Task_1_1_3/src/test/java/ru/nsu/aeliseev2/task113/tests/ExpressionParseTests.java
@@ -0,0 +1,137 @@
+package ru.nsu.aeliseev2.task113.tests;
+
+import java.text.ParseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.nsu.aeliseev2.task113.expressions.Addition;
+import ru.nsu.aeliseev2.task113.expressions.Division;
+import ru.nsu.aeliseev2.task113.expressions.Expression;
+import ru.nsu.aeliseev2.task113.expressions.Multiplication;
+import ru.nsu.aeliseev2.task113.expressions.Negation;
+import ru.nsu.aeliseev2.task113.expressions.Number;
+import ru.nsu.aeliseev2.task113.expressions.Subtraction;
+import ru.nsu.aeliseev2.task113.expressions.Variable;
+
+class ExpressionParseTests {
+    @Test
+    void variable() throws ParseException {
+        String string = "  hello_World ";
+        var expected = new Variable("hello_World");
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void integerNumber() throws ParseException {
+        String string = "256";
+        var expected = new Number(256);
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void fractionalNumber() throws ParseException {
+        String string = "256.360";
+        var expected = new Number(256.360);
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void addition() throws ParseException {
+        String string = "1 + 2";
+        var expected = new Addition(new Number(1), new Number(2));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void operatorPrecedence1() throws ParseException {
+        String string = "1*2+3";
+        var expected = new Addition(
+            new Multiplication(new Number(1), new Number(2)),
+            new Number(3));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void operatorPrecedence2() throws ParseException {
+        String string = "1+2*3";
+        var expected = new Addition(
+            new Number(1),
+            new Multiplication(new Number(2), new Number(3)));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void negatedMultiplication() throws ParseException {
+        String string = "-1 * -1";
+        var expected = new Multiplication(
+            new Negation(new Number(1)),
+            new Negation(new Number(1)));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void negatedSubtraction() throws ParseException {
+        String string = "-1---1";
+        var expected = new Subtraction(
+            new Negation(new Number(1)),
+            new Negation(new Negation(new Number(1))));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void parentheses1() throws ParseException {
+        String string = "(1 + 2) * 3";
+        var expected = new Multiplication(
+            new Addition(new Number(1), new Number(2)),
+            new Number(3));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void parentheses2() throws ParseException {
+        String string = "5 * (1 + 2)";
+        var expected = new Multiplication(
+            new Number(5),
+            new Addition(
+                new Number(1),
+                new Number(2)));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void addAssociativity() throws ParseException {
+        String string = "1 + 2 - 3 + 4";
+        var expected = new Addition(
+            new Subtraction(
+                new Addition(
+                    new Number(1),
+                    new Number(2)),
+                new Number(3)),
+            new Number(4));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void mulAssociativity() throws ParseException {
+        String string = "1 * 2 / 3 * 4";
+        var expected = new Multiplication(
+            new Division(
+                new Multiplication(
+                    new Number(1),
+                    new Number(2)),
+                new Number(3)),
+            new Number(4));
+        var actual = Expression.fromString(string);
+        Assertions.assertEquals(expected, actual);
+    }
+}
