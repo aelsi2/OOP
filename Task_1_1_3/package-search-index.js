ad1ab68f8d6d81c240f9bc088fe140fd8c47c224
aelsi2 <aelsi2@users.noreply.github.com>
2025-09-10 09:31:19 +0000

    Deploying to gh-pages from @ aelsi2/OOP@b6420225e60d6ed436bece9446a5479ba6f5023c 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.aeliseev2.task113"},{"l":"ru.nsu.aeliseev2.task113.expressions"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.aeliseev2.task113"},{"l":"ru.nsu.aeliseev2.task113.expressions"},{"l":"ru.nsu.aeliseev2.task113.parsers"}];updateSearchResults();
