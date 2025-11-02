81e9ae047698af795fd4d487ea0d9e64eba04885
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2025-11-03 01:09:31 +0700

    Revert "consider dead state when getting the state of a cell"
    This reverts commit 002663b953f6cb960711c0e3c4b28fd28b904cb6.

--- Task_1_2_2/src/main/java/ru/nsu/aeliseev2/task122/HashTable.java
+++ Task_1_2_2/src/main/java/ru/nsu/aeliseev2/task122/HashTable.java
@@ -48,7 +48,7 @@ public class HashTable<K, V> implements Map<K, V> {
 
         @Override
         public V getValue() {
-            return isDead ? null : value;
+            return value;
         }
 
         @Override
