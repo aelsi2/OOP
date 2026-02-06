bf1691985162c70040690b3f02b31ee408dc4057
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2026-02-07 00:35:03 +0700

    make HasCompositesStreamTests private
    (to make review dog shut up)

--- Task_2_1_1/src/test/java/ru/nsu/aeliseev2/task211/HasCompositesStreamTests.java
+++ Task_2_1_1/src/test/java/ru/nsu/aeliseev2/task211/HasCompositesStreamTests.java
@@ -1,6 +1,6 @@
 package ru.nsu.aeliseev2.task211;
 
-public class HasCompositesStreamTests extends HasCompositesTests {
+class HasCompositesStreamTests extends HasCompositesTests {
     @Override
     protected PrimeChecker getChecker() {
         return new ParallelStreamPrimeChecker();
