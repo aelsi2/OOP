daca82c5a821e0b2a305870780900f2ec3163dad
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2026-03-02 09:54:42 +0700

    add restaurant configuration class

---
+++ Task_2_2_1/src/main/java/ru/nsu/aeliseev2/task221/RestaurantConfig.java
@@ -0,0 +1,110 @@
+package ru.nsu.aeliseev2.task221;
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Restaurant configuration class intended for serialization.
+ */
+public class RestaurantConfig {
+    /**
+     * Employee configuration class.
+     */
+    public static class EmployeeConfig {
+        /**
+         * The minimum amount of time in milliseconds spent working on each iteration.
+         */
+        public int minDelayMillis;
+
+        /**
+         * The maximum amount of time in milliseconds spent working on each iteration.
+         */
+        public int maxDelayMillis;
+    }
+
+    /**
+     * Delivery man configuration class.
+     */
+    public static class DeliveryManConfig extends EmployeeConfig {
+        /**
+         * The maximum number of orders the delivery man is able to carry at the same time.
+         */
+        public int capacity;
+    }
+
+    /**
+     * The order takers working in this restaurant.
+     */
+    public EmployeeConfig[] orderTakers;
+
+    /**
+     * The bakers working in this restaurant.
+     */
+    public EmployeeConfig[] bakers;
+
+    /**
+     * The delivery men working in this restaurant.
+     */
+    public DeliveryManConfig[] deliveryMen;
+
+    /**
+     * The capacity of the taker-baker order queue.
+     */
+    public int toBakeCapacity;
+
+    /**
+     * The capacity of the baker-deliverer order queue.
+     */
+    public int toDeliverCapacity;
+
+    /**
+     * The number of orders the restaurant will take.
+     */
+    public int orderCount;
+
+    /**
+     * Parses a {@code RestaurantConfig} from a JSON string.
+     *
+     * @param json The JSON string to parse from.
+     * @return The parsed {@code RestaurantConfig}.
+     */
+    public static RestaurantConfig fromJson(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, RestaurantConfig.class);
+    }
+
+    /**
+     * Creates a new {@code Restaurant} based on this configuration.
+     *
+     * @return The created restaurant.
+     */
+    public Restaurant createRestaurant() {
+        final OrderFactory orderFactory = new SequentialOrderFactory(orderCount);
+        final OrderQueue toBake = new OrderQueue(toBakeCapacity);
+        final OrderQueue toDeliver = new OrderQueue(toDeliverCapacity);
+        final OrderLogger logger = new PrintStreamOrderLogger(System.out);
+        final ArrayList<Runnable> employees = new ArrayList<>();
+
+        for (EmployeeConfig orderTaker : orderTakers) {
+            final WorkingStrategy strategy =
+                new RandomDelayWorkingStrategy(new Random(),
+                    orderTaker.minDelayMillis, orderTaker.maxDelayMillis);
+            employees.add(new OrderTaker(orderFactory, toBake, logger, strategy));
+        }
+        for (EmployeeConfig baker : bakers) {
+            final WorkingStrategy strategy =
+                new RandomDelayWorkingStrategy(new Random(),
+                    baker.minDelayMillis, baker.maxDelayMillis);
+            employees.add(new Baker(toBake, toDeliver, logger, strategy));
+        }
+        for (DeliveryManConfig deliveryMan : deliveryMen) {
+            final WorkingStrategy strategy =
+                new RandomDelayWorkingStrategy(new Random(),
+                    deliveryMan.minDelayMillis, deliveryMan.maxDelayMillis);
+            employees.add(new DeliveryMan(toDeliver, deliveryMan.capacity, logger, strategy));
+        }
+
+        return new Restaurant(employees);
+    }
+}
