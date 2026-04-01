e3bdfd7da08a698ed43adaef086552508f5ca6ab
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2026-04-02 01:14:35 +0700

    put palettes into a separate file

--- Task_2_3_1/app/src/main/java/ru/nsu/aeliseev2/task231/app/SnakeController.java
+++ Task_2_3_1/app/src/main/java/ru/nsu/aeliseev2/task231/app/SnakeController.java
@@ -1,26 +1,9 @@
 package ru.nsu.aeliseev2.task231.app;
 
 import javafx.fxml.FXML;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import ru.nsu.aeliseev2.task231.app.drawing.FillRectTile;
-import ru.nsu.aeliseev2.task231.app.drawing.FoodTile;
-import ru.nsu.aeliseev2.task231.app.drawing.SnakeBodyTile;
-import ru.nsu.aeliseev2.task231.app.drawing.SnakeHeadTile;
-import ru.nsu.aeliseev2.task231.app.drawing.Tile;
 import ru.nsu.aeliseev2.task231.app.drawing.TileMap;
-import ru.nsu.aeliseev2.task231.model.SnakeModel;
 
 public class SnakeController {
-    private static final Tile[] BG_PALETTE = new Tile[]{
-        new FillRectTile(Paint.valueOf("#5fa34f")),
-        new FillRectTile(Paint.valueOf("#70b262")),
-    };
-    private static final Tile[] FG_PALETTE = makeForegroundPalette(
-        Paint.valueOf("#53a4e2"),
-        Paint.valueOf("#3887c2"),
-        Paint.valueOf("#eb5146"));
-
     @FXML
     private TileMap background;
 
@@ -32,26 +15,10 @@ public class SnakeController {
         background.setData(makeBackground(10, 10));
         background.setMapWidth(10);
         background.setMapHeight(10);
-        background.setPalette(BG_PALETTE);
+        background.setPalette(Palettes.BACKGROUND_GRASS);
         foreground.setMapWidth(10);
         foreground.setMapHeight(10);
-        foreground.setPalette(FG_PALETTE);
-    }
-
-    private static Tile[] makeForegroundPalette(
-        Paint snakePrimary,
-        Paint snakeSecondary,
-        Paint food) {
-        Tile[] palette = new Tile[10];
-        palette[0] = new FillRectTile(Color.TRANSPARENT);
-        for (int i = 0; i < 4; i++) {
-            palette[1 + i] = new SnakeBodyTile(
-                snakePrimary, snakeSecondary, 180 - i * 90);
-            palette[1 + i + 4] = new SnakeHeadTile(
-                snakePrimary, snakeSecondary, 180 - i * 90);
-        }
-        palette[9] = new FoodTile(food);
-        return palette;
+        foreground.setPalette(Palettes.FOREGROUND_BLUE_RED);
     }
 
     private static int[] makeBackground(int width, int height) {
