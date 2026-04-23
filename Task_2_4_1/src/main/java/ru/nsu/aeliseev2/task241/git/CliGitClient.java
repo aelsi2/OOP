35474bfae77a0e2a668ff94c21d83eb60fe672b1
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2026-04-23 16:36:02 +0700

    add git client impl

---
+++ Task_2_4_1/src/main/java/ru/nsu/aeliseev2/task241/git/CliGitClient.java
@@ -0,0 +1,51 @@
+package ru.nsu.aeliseev2.task241.git;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * An implementation of {@code GitClient} that uses the Git CLI tool directly.
+ */
+public class CliGitClient implements GitClient{
+    private final String gitCommand;
+
+    /**
+     * Initializes a new instance of {@code CliGitClient} using the default command {@code git}.
+     */
+    public CliGitClient() {
+        this("git");
+    }
+
+    /**
+     * Initializes a new instance of {@code CliGitClient}.
+     *
+     * @param gitCommand The Git command to use.
+     */
+    public CliGitClient(String gitCommand) {
+        this.gitCommand = gitCommand;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GitRepo clone(String uri) {
+        try {
+            Path tempDir = Files.createTempDirectory("aelsi2_task241_");
+
+            ProcessBuilder pb = new ProcessBuilder();
+            pb.environment().put("GIT_TERMINAL_PROMPT", "0");
+            pb.command(gitCommand, "clone", uri, tempDir.toString());
+            Process process = pb.start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("Git returned non-zero exit code");
+            }
+            return new CliGitRepo(gitCommand, tempDir);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Task_2_4_1/src/main/java/ru/nsu/aeliseev2/task241/git/CliGitRepo.java
@@ -0,0 +1,100 @@
+package ru.nsu.aeliseev2.task241.git;
+
+import com.google.common.io.MoreFiles;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * An implementation of {@code GitRepo} that uses the Git CLI tool directly.
+ */
+public class CliGitRepo implements GitRepo {
+    private final Path repoDirectory;
+    private final String gitCommand;
+
+    /**
+     * Initializes a new instance of {@code CliGitRepo}.
+     *
+     * @param gitCommand    The Git command name.
+     * @param repoDirectory The directory the repository is located in.
+     */
+    public CliGitRepo(String gitCommand, Path repoDirectory) {
+        this.gitCommand = gitCommand;
+        this.repoDirectory = repoDirectory.toAbsolutePath();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> branches() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder();
+            pb.environment().put("GIT_TERMINAL_PROMPT", "0");
+            pb.command(gitCommand, "-C", repoDirectory.toString(),
+                "branch", "--format", "%(refname:short)");
+            Process process = pb.start();
+
+            List<String> result = new ArrayList<>();
+            try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.add(line);
+                }
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("Git returned non-zero exit code");
+            }
+            return result;
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void checkout(String branch) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder();
+            pb.environment().put("GIT_TERMINAL_PROMPT", "0");
+            pb.command(gitCommand, "-C", repoDirectory.toString(),
+                "checkout", branch);
+            Process process = pb.start();
+
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new RuntimeException("Git returned non-zero exit code");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Path directory() {
+        return repoDirectory;
+    }
+
+    /**
+     * Deletes the repository.
+     */
+    @Override
+    public void close() {
+        try {
+            MoreFiles.deleteRecursively(repoDirectory);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
