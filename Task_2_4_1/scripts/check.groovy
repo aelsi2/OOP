7d4aa550200b4c8ac16e419b413cd9311726e767
Andrey Eliseev <a.eliseev2@g.nsu.ru>
2026-04-24 23:07:16 +0700

    make the DSL a little less insane

--- Task_2_4_1/scripts/check.groovy
+++ Task_2_4_1/scripts/check.groovy
@@ -2,6 +2,6 @@ include "tasks.groovy"
 include "groups.groovy"
 
 review("2025-09-12") {
-    accept("Task_1_1_1", hard) extra 2 forStudent "aelsi2"
-    accept("Task_1_1_1", hard) forStudent "7AD0VNIK"
+    hardAccept "Task_1_1_1" extra 2 forStudent "aelsi2"
+    hardAccept "Task_1_1_1" forStudent "7AD0VNIK"
 }
